b851489c27a911a74f5d275b0b0f0cb654195448
DeimisM <117855291+DeimisM@users.noreply.github.com>
2025-04-04 21:04:31 +0300

    Starting the generator

--- main.cpp
+++ main.cpp
@@ -3,22 +3,53 @@
 #include <iostream> // keyboard and screen
 #include <cmath> // math funcitions, e.g. sqr root, etc.
 
+#include <cctype>
+
 using namespace std;
 
-
-int test ()
+int generator ()
 {
-    cout << "hi\n";
+    cout << "\n\nWelcome to the generator\n\n";
+
+
+
     return 0;
 }
 
 int main ()
 {
-    cout << "\n\nWelcome to my password generator!\n\n";
 
-    test();
+    string yesOrNo;
 
-    cout << "finished\n";
+    cout << "\n\nWelcome to my password generator!\n";
+    cout << "Would you like to start generating passwords? (Y/n)\n\n";
+
+    //cin >> yesOrNo;
+    getline(cin, yesOrNo);  // doesnt wait for input and takes the entire line
+
+
+                                                    // if input == y, then passwd generator is started
+    if (!yesOrNo.empty() && tolower(yesOrNo[0]) == 'y')
+    {
+        generator();
+    }
+                                                    // if input == null, then the program automatically assumes the input is "Y"
+    else if (yesOrNo == "")
+    {
+        generator();
+    }
+                                                    // if input == n, then program terminates itself
+    else if (!yesOrNo.empty() && tolower(yesOrNo[0]) == 'n')
+    {
+        cout << "\n\nExiting the program.\n\n";
+        exit(0);
+    }
+    else
+    {
+
+        cout << "\n\nYou have failed to enter the program. Shutting down.\n\n";
+        exit(0);
+    }
 
     return 0;
 }
