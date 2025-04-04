a3d87b1205bd4f642007b8ccc5bdb3cc9af4bf8b
DeimisM <117855291+DeimisM@users.noreply.github.com>
2025-04-04 20:28:21 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <fstream> // work with txt files
+#include <iomanip> // skaiciu apvalinimas
+#include <iostream> // keyboard and screen
+#include <cmath> // math funcitions, e.g. sqr root, etc.
+
+using namespace std;
+
+
+int test ()
+{
+    cout << "hi\n";
+    return 0;
+}
+
+int main ()
+{
+    cout << "\n\nWelcome to my password generator!\n\n";
+
+    test();
+
+    cout << "finished\n";
+
+    return 0;
+}
