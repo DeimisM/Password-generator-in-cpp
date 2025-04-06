1476e2db1391a5c1bd210bdfd2de6e56bd045b7a
DeimisM <117855291+DeimisM@users.noreply.github.com>
2025-04-06 17:13:51 +0300

    int main fix
    fixed input processing, now the program will ask you until it gets what it wants :)

--- main.cpp
+++ main.cpp
@@ -11,26 +11,24 @@ int generator ()
 {
     int passwdLength = 0;
 
-    cout << "\n\nWelcome to the generator\n";
-
-    while (true)
+    while (true)        // for asking for an input you require indefinitely
     {
-        cout << "\n\nPlease type the desired length of your generated password:\n\n";
+        cout << "\n\nPlease type the desired length of your generated password:\n";
         cin >> passwdLength;
-
-        if (cin.fail())
+                                            // if != number:
+        if (cin.fail())                         // jam clearance:
         {
             cin.clear();                            // clear the fail state
             cin.ignore(1000, '\n');                 // discard the invalid input
-            cout << "\nInvalid input. Please enter a number.\n";
+            cout << "\n\nInvalid input. Please enter a number.\n";
             continue;                               // go back to the top of the loop
         }
 
-        cout << passwdLength;
-
+        //cout << passwdLength;
+                                            // if number is <= 0
         if (passwdLength <= 0)
         {
-            cout << "\nPlease enter a number greater than 0\n";
+            cout << "\n\nPlease enter a number greater than 0.\n";
             continue;
         }
 
@@ -42,38 +40,33 @@ int generator ()
 
 int main ()
 {
-
     string yesOrNo;
 
-    cout << "\n\nWelcome to my password generator!\n";
-    cout << "Would you like to start generating passwords? (Y/n)\n\n";
+    cout << "\n\nWelcome to my password generator!";
 
-    //cin >> yesOrNo;
-    getline(cin, yesOrNo);  // doesnt wait for input and takes the entire line after Enter
-
-
-                                                    // if input == y, then passwd generator is started
-    if (!yesOrNo.empty() && tolower(yesOrNo[0]) == 'y')
+    while (true)        // for asking for an input you require indefinitely
     {
-        generator();
-    }
-                                                    // if input == null, then the program automatically assumes the input is "Y"
-    else if (yesOrNo == "")
-    {
-        generator();
-    }
-                                                    // if input == n, then program terminates itself
-    else if (!yesOrNo.empty() && tolower(yesOrNo[0]) == 'n')
-    {
-        cout << "\n\nExiting the program.\n\n";
-        exit(0);
-    }
-    else
-    {
+        cout << "\n\nWould you like to start generating passwords? (Y/n)\n";
+        //cin >> yesOrNo;
+        getline(cin, yesOrNo);                      // doesnt wait for input and takes the entire line after Enter (better option for this case instead of cin >> yesOrNo)
 
-        cout << "\n\nYou have failed to enter the program. Shutting down.\n\n";
-        exit(0);
+        //if (yesOrNo == NULL || yesOrNo == "")     // in c++ you cant do this
+        if (yesOrNo.empty())                        // if input == null, then the program automatically assumes the input is "Y" because of (Y/n)
+        {
+            generator();
+            break;
+        }
+        else if (tolower(yesOrNo[0]) == 'y')        // lowers caps, takes the first char of input and compares it to required result for the action
+        {
+            generator();
+            break;
+        }
+        else if (tolower(yesOrNo[0]) == 'n')        // if input == n, then program terminates itself
+        {
+            cout << "\n\nHave a nice day!\n";
+            exit(0);
+        }
     }
-
     return 0;
 }
+
